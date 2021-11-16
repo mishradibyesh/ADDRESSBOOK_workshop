989c054d8b1e1b8c0929cd7aebfa6fb0f0c191a2
mishradibyesh <dibyeshmishra207@gmail.com>
2021-11-16 07:58:42 +0530

    welcome message

--- com/addressbook/AddressBookMain.java
+++ com/addressbook/AddressBookMain.java
@@ -3,8 +3,8 @@ package com.addressbook;
 public class AddressBookMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		System.out.println("Welcome to addressBook program");
 	}
 
 }
